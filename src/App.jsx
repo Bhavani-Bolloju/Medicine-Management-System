9c5bc13312dc3a16c02ba0a0921aee4663c3386d
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2025-03-20 23:23:25 +0530

    initialize project using Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="p-5">
+      <h1 className="text-2xl">Medicine management system</h1>
+     
+    </div>
+  )
+}
+
+export default App
